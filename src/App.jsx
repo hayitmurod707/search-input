c0ba34943d906049cacff028f58d5bd2b29d1d27
hayitmurod707 <hayitmurod707@gmail.com>
2022-07-28 17:46:20 +0500

    created SearchInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-const App = () => {
-	return (
-		<div className='App'>
-			<h1>App</h1>
+import React from 'react';
+import styled from 'styled-components';
+import SearchInput from './SearchInput';
+const StyledElement = styled.section`
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	& .column {
+		margin: 100px 0 0 0;
+		width: 400px;
+	}
+`;
+const App = () => (
+	<StyledElement>
+		<div className="column">
+			<SearchInput />
 		</div>
-	);
-};
+	</StyledElement>
+);
 export default App;
